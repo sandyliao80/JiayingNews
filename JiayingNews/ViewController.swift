75f2c12e55c49a24c9ba2fe55cc3cb4f20b6b2dc
LOO2K <loo2km@gmail.com>
2014-10-21 22:35:46 +0800

    Initial Commit

---
+++ JiayingNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JiayingNews
+//
+//  Created by LOO2K on 14/10/21.
+//  Copyright (c) 2014年 luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
